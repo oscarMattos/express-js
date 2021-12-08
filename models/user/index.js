1525eb89625762a15e4032fcdd17165be94b511f
Oscar <oscar.mattos@globant.com>
2021-12-08 01:36:00 -0300

    Added duplicated email validation on user model

--- models/user/index.js
+++ models/user/index.js
@@ -2,20 +2,26 @@ const mongoose = require("mongoose");
 
 const User = mongoose.Schema({
   name: {
-    type: String,
     required: true,
+    type: String,
   },
   email: {
+    required: [true, "User email required"],
     type: String,
-    required: true,
+    unique: true,
+    validate: {
+      validator: (value) =>
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value),
+      message: (props) => `${props.value} is not a valid email!`,
+    },
   },
   password: {
-    type: String,
     required: true,
+    type: String,
   },
   date: {
-    type: Date,
     default: Date.now(),
+    type: Date,
   },
 });
 
