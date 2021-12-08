cde48b158b745a34524eff3801c971bdc06f2bb5
Oscar <oscar.mattos@globant.com>
2021-12-08 03:03:33 -0300

    Added improvement

--- routes/token/index.js
+++ routes/token/index.js
@@ -9,7 +9,7 @@ router.route("/token").get((req, res) => {
   let token = jwt.sign({ _id: "token" }, process.env.SECRET);
   token
     ? res.send(token)
-    : res.send("the token could not be created. Please, try again.");
+    : res.send("Token could not be created. Please, try again.");
 });
 
 module.exports = router;
