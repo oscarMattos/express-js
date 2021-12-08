470e9cedab1fb5be353fb46393370404de226ad9
Oscar <oscar.mattos@globant.com>
2021-12-08 00:16:59 -0300

    Added code improvements

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ const users = require("./routes/users");
 app.use("/api", users);
 
 // Server
-app.listen("3000", () => {
-  console.log("Sever is running!!!!!!");
+app.listen("3000", (err, res) => {
+  err
+    ? console.log(`Server Conexion Error - ${err}`)
+    : console.log("Sever is running");
 });
 
 // Data Base Conexion
-mongoose.connect(process.env.DB_URL, (err) =>
+mongoose.connect(process.env.DB_URL, (err, res) =>
   err
     ? console.log(`Data Base Conexion Error - ${err}`)
     : console.log("Data Base Conected")
